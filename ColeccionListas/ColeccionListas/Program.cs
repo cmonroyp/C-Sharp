fe19b009a36793098f86dbaffa1a80f27a26da8c
cmonroyp <cmonroy2@gmail.com>
2019-04-19 14:52:10 -0500

    Listas

---
+++ ColeccionListas/ColeccionListas/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ColeccionListas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //diferencia entre arreglo y lista
+            //arreglo se tiene que espeificar su dimension
+            int[] numerosArreglo = new int[10];
+
+            //listas no se debe especificar su dimension
+            ListarNumeros();
+            Console.WriteLine("*******Agrega Numero*********");
+            InsertaNumero();
+            Console.WriteLine("*******Borra Numero*********");
+            DeleteNumero();
+            Console.WriteLine("*******Iterando lista for*********");
+            IterandoLista();
+
+            Console.ReadLine();
+        }
+
+        public static void ListarNumeros()
+        {
+            List<int> listNumeros = new List<int>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                listNumeros.Add(i);
+            }
+
+            foreach (var item in listNumeros)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void InsertaNumero()
+        {
+            List<int> listNumeros = new List<int>();
+            for (int i = 1; i <= 10; i++)
+            {
+                listNumeros.Add(i);
+            }
+
+            //inserta numero en la penultima posicion
+            listNumeros.Insert(listNumeros.Count - 1, 15);
+
+            foreach (var item in listNumeros)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+        public static void DeleteNumero()
+        {
+            List<int> listNumeros = new List<int>();
+            for (int i = 1; i <= 10; i++)
+            {
+                listNumeros.Add(i);
+            }
+
+            //borra el penultimo numero
+            listNumeros.Remove(listNumeros.Count - 1);
+
+            //borra un numero especifico de cierta posicion
+            listNumeros.RemoveAt(3);
+
+            //borra un rango de numeros de la lista
+            listNumeros.RemoveRange(0, 2);
+
+            //si deseo limpiar toda la lista
+            //listNumeros.Clear();
+
+            foreach (var item in listNumeros)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+        //iterando lista generica
+        public static void IterandoLista()
+        {
+            List<int> listNumeros = new List<int>();
+            for (int i = 1; i <= 10; i++)
+            {
+                listNumeros.Add(i);
+            }
+
+            for (int i = 0; i < listNumeros.Count; i++)
+            {
+                int item = listNumeros[i];
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ CollectionLinkedList/CollectionLinkedList/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace CollectionLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AgregarNumero();
+            Console.WriteLine("******agrega numeros al inicio de la lista");
+            AgregarNumero2();
+
+            Console.ReadLine();
+        }
+
+        public static void AgregarNumero()
+        {
+            LinkedList<int> numeros = new LinkedList<int>();
+
+            //numeros.AddLast(3);
+            //agrega numeros al final de la lista
+            for (int i = 1; i <= 10; i++)
+            {
+                numeros.AddLast(i);
+            }
+
+            foreach (var item in numeros)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void AgregarNumero2()
+        {
+            LinkedList<int> numeros = new LinkedList<int>();
+
+            //agrega un numero despues de un node especifico
+            var num = numeros.AddFirst(150);
+            numeros.AddAfter(num, 200);
+
+            //agregarlo antes del node especifico
+            numeros.AddBefore(num, 140);
+
+            //agrega numeros al inicio de la lista
+            for (int i = 1; i <= 10; i++)
+            {
+                numeros.AddFirst(i);
+            }
+
+            foreach (var item in numeros)
+            {
+                Console.WriteLine(item);
+            }
+
+            //imprime el primer node
+            var nodo = numeros.First;
+            Console.WriteLine("primer nodo: " + nodo.Value);
+
+            //metodo de elimmincacon
+            //numeros.Remove(3);
+            //numeros.RemoveFirst();
+            //numeros.RemoveLast();
+
+        }
+    }
+}
