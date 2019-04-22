60793c05b130c4f55a308ce3834b53137acec271
cmonroyp <cmonroy2@gmail.com>
2019-04-21 23:47:43 -0500

    sistema de negocio

---
+++ SistemaAsistencia/Capa_Datoss/BD_Conexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Capa_Datos
+{
+    public class BD_Conexion
+    {
+        public string ConectarBD()
+        {
+            var con = ConfigurationManager.ConnectionStrings["ConBD"].ConnectionString;
+
+            return con;
+        }
+    }
+}
